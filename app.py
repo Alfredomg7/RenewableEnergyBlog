1d88135aba760bea781cdc3eb8000af100b9b944
Alfredomg7 <alfredomg4000@gmail.com>
2023-11-30 10:10:49 -0800

    Implemented Message model for contact form processing and added route definitions

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blog Platform Flask Application"""
 
 # Import necesary libraries
-from flask import Flask
+from flask import Flask, flash, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from dotenv import load_dotenv
@@ -16,7 +16,7 @@ load_dotenv()
 
 # Configure database
 base_dir = os.path.dirname(os.path.realpath(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(base_dir, 'ze_blog.db')
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(base_dir, 'renewable_energy_blog.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
 
@@ -49,3 +49,60 @@ class Article(db.Model):
 
     def __repr__(self):
         return f"Article: <{self.title}>"
+
+# Define 'Message' class to store contact form messages in the 'messages' table
+class Message(db.Model):
+    __tablename__ = "messages"
+    id = db.Column(db.Integer, primary_key=True)
+    sender = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+    title = db.Column(db.String(80), nullable=False)
+    message = db.Column(db.String, nulllable=False)
+    priority = db.Column(db.String(20))
+
+    def __repr__(self):
+        return f"Message: <{self.title}>"
+    
+# Initialize and create database tables from models if they don't already exist
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+# Define route for the homepage, displaying a list of articles
+@app.route('/')
+def index():
+    articles = Article.query.all()
+    context = {
+        "articles": articles
+    }
+    return render_template('index.html', **context)
+
+# Route definition for the 'About' page
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Define route for contact form, handling both display (GET) and data submission (POST)
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        # Extracting data from the contact form
+        sender = request.form.get('name')
+        email = request.form.get('email')
+        title = request.form.get('title')
+        message = request.form.get('message')
+        priority = request.form.get('priority')
+
+        # Creating a new message record and saving it to the database
+        new_message = Message(sender=sender, email=email,
+                                title=title, message=message,
+                                priority=priority)
+        db.session.add(new_message)
+        db.session.commit()
+
+        # Notification for successful message submission
+        flash("Message sent. Thanks for reaching out!")
+        return redirect(url_for('index'))
+    
+    # Render the contact form template on GET request
+    return render_template('contact.html')
